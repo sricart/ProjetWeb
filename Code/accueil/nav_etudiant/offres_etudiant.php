636bb22ad7613c2c391f8b3e42068a020587be39
sricart <sebastien.ricart@viacesi.fr>
2023-03-16 22:59:04 +0100

    V8
    Ajout du logo, des sous barres et du css pour le footer

--- Code/accueil/nav_etudiant/offres_etudiant.php
+++ Code/accueil/nav_etudiant/offres_etudiant.php
@@ -4,35 +4,25 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/style.css">
+        <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
-        <title> À Propos </title>
+        <title> Entreprises & Offres </title>
     </head>
     <body>
     <header>
         <div class="logo">LOGO</div>
         <nav class="nav-bar">
-            <ul>
+        <ul>
                 <li>
                     <a href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/accueil_etudiant.php">Accueil</a> 
                 </li>
                 <li>
                     <a href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/liste_souhaits.php">Liste de souhaits</a> 
                 </li>
-                    <li>
-                        <a>Entreprises et Offres</a> 
-                        <div class="sous-menu">
-                            <ul>
-                                <li>
-                                    <a href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/entreprises_etudiant.php" > Entreprises </a>
-                                </li>
-                                <li>
-                                    <a href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/offres_etudiant.php" > Offres </a>
-                                </li>
-                            </ul>
-                        </div>  
-                    </li>
+                <li>
+                <a href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/entreprises&offres_etudiant.php">Entreprises et Offres</a> 
+                </li>
                 <li>
                     <a href="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/compte_etudiant.php">Votre compte</a> 
                 </li>
@@ -60,6 +50,5 @@
         </ul>
     </footer>
 
-    <script src="http://localhost/B4/Projet/L3/code/accueil/nav_etudiant/"> </script>
     </body>
 </html>
