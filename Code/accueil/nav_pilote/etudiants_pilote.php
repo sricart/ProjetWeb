a1b3e5745828af03de79b6c0eff384198c0ce7a4
sricart <sebastien.ricart@viacesi.fr>
2023-03-19 19:25:06 +0100

    V12
    ajout du tableau crud au pilote sans css

--- Code/accueil/nav_pilote/etudiants_pilote.php
+++ Code/accueil/nav_pilote/etudiants_pilote.php
@@ -1,3 +1,20 @@
+<?php
+    // On démarre une session
+    session_start();
+    // On inclut la connexion à la base
+    require_once('CRUD_Etudiant/connect.php');
+    // Selectionne les infos importantes pour l'admin concernant les etudiants donc admin et pilote !=1
+    $sql = 'SELECT `Id_Etudiant`,`N_Etudiant`,`P_Etudiant`,`Cv`,`Lettre_Motivation`,`Photo`,`Id_Promotion`,`Login`,`Mdp` FROM etudiant INNER JOIN authentifiant ON etudiant.ID_Auth = authentifiant.ID_Auth WHERE `Admin`!="1" AND `Pilote`!="1";';
+    // On prépare la requête
+    $query = $db->prepare($sql);
+    // On exécute la requête
+    $query->execute();
+    // On stocke le résultat dans un tableau associatif
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    require_once('CRUD_Etudiant/close.php');
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,6 +24,7 @@
         <link rel="stylesheet" href="http://localhost/B4/Projet/L3/code/accueil/nav_admin/style.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
         <title> Etudiant </title>
         <script>
             function afficherInfo() {
@@ -20,7 +38,7 @@
         </script>
     </head>
     <body>
-    <header>
+        <header>
             <div class="logo"> <img src="http://localhost/B4/projet/L3/code/image/logo.png">
             </div>
             <div class="search-bar">
@@ -59,24 +77,73 @@
             </nav>
         </header>
 
-        Bienvenue sur la page pilote
+        <h1>Liste des étudiants</h1>
         <br>
+        <?php
+                    if(!empty($_SESSION['erreur'])){
+                        echo '<div class="alert alert-danger" role="alert">
+                                '. $_SESSION['erreur'].'
+                            </div>';
+                        $_SESSION['erreur'] = "";
+                    }
+                ?>
+                <?php
+                    if(!empty($_SESSION['message'])){
+                        echo '<div class="alert alert-success" role="alert">
+                                '. $_SESSION['message'].'
+                            </div>';
+                        $_SESSION['message'] = "";
+                    }
+                ?>
         <input type="button" onclick="afficherInfo()" value="AFFICHER">
         <br>
-        <div id="infos" style="display:none">
-            <?php
-            $user='root';
-            $pass='';
-            $bd='projet';
-            $bd=new mysqli('localhost', $user, $pass, $bd) or die ("unable to connect");
-            $sql="SELECT * FROM etudiant";
-            $result=mysqli_query($bd,$sql) or die ("bad query");
-            while($row=mysqli_fetch_assoc($result))
-            {
-                echo"{$row['Id_Etudiant']} - {$row['N_Etudiant']} - {$row['P_Etudiant']} - {$row['Statut_Stage']} - {$row['Cv']} - {$row['Lettre_Motivation']} 
-                - {$row['Photo']} - {$row['Id_Promotion']}  - {$row['Id_Pilote']} - {$row['Id_Auth']} <br> ;";
-            }
-            ?>
+
+        <div id="infos" class="container" style="display:none;" >
+        <div class="row">
+            <section class="col-12">
+                
+                <table class="table">
+                    <thead>
+                        <th>ID</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>CV</th>
+                        <th>Lettre de motivation</th>
+                        <th>Photo</th>
+                        <th>Promotion</th>
+                        <th>Email</th>
+                        <th>Mot de passe</th>
+                        <th></th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        <br>
+                        <a href="http://localhost/B4/projet/L3/Code/accueil/nav_pilote/CRUD_Etudiant/addAuthEtudiant.php" class="btn btn-primary">Ajouter un étudiant</a>
+                        <?php
+                        // On boucle sur la variable result
+                        foreach($result as $etudiant){
+                        ?>
+                        <br>
+                            <tr>
+                                <td><?= $etudiant['Id_Etudiant'] ?></td>
+                                <td><?= $etudiant['N_Etudiant'] ?></td>
+                                <td><?= $etudiant['P_Etudiant'] ?></td>
+                                <td><?= $etudiant['Cv'] ?></td>
+                                <td><?= $etudiant['Lettre_Motivation'] ?></td>
+                                <td><?= $etudiant['Photo'] ?></td>
+                                <td><?= $etudiant['Id_Promotion'] ?></td>
+                                <td><?= $etudiant['Login'] ?></td>
+                                <td><?= $etudiant['Mdp'] ?></td>
+                                <td><a href="http://localhost/B4/projet/L3/Code/accueil/nav_pilote/CRUD_Etudiant/editEtudiant.php?Id_Etudiant=<?= $etudiant['Id_Etudiant'] ?>"><i class="fa duotone fa-pencil"></i></a></td> 
+                                <td><a href="http://localhost/B4/projet/L3/Code/accueil/nav_pilote/CRUD_Etudiant/deleteEtudiant.php?Id_Etudiant=<?= $etudiant['Id_Etudiant'] ?>"><i class="fa solid fa-trash"></i></a></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </section>
+            </div>
         </div>
        
         <footer>
