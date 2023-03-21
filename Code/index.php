5795f5a21e1bb65fbf8908dd2846e9046ed4075c
sricart <sebastien.ricart@viacesi.fr>
2023-03-21 13:54:25 +0100

    V15
    Modification de lien + ajout de css à certaines pages

--- Code/index.php
+++ Code/index.php
@@ -44,10 +44,10 @@ if(isset($_POST['submit'])){
             header('location: http://localhost/code/accueil/nav_etudiant/accueil_etudiant.php');
             exit;
         } else {
-            $error = "Login et/ou mot de passe incorrect";
+           
         }
     } else {
-        $error = "Login et/ou mot de passe incorrect";
+       
     }
 }
 ?>
@@ -78,6 +78,7 @@ if(isset($_POST['submit'])){
                     <div class="user-box">
                         <input type="password" name="pass" required="">
                         <label>Mot de passe</label>
+                        <a class="mdp_oublie" href="mdp_oublie.php"> Mot de passe oublié ? </a>
                     </div>
                     <a >
                         <input class="button" type="submit" name="submit" value="Connexion">
@@ -120,19 +121,3 @@ if(isset($_POST['submit'])){
     </body>
     
 </html>
-
-
-
-
-
-
-
-
-<?php
-    /* Vérifier si Gandalf est dans la BDD 
-    $req_str = "SELECT * FROM utilisateurs WHERE pseudo = 'Gandalf';";
-    $stmt = $conn->query($req_str);
-    if(!$stmt){echo "erreur de requête : $req_str\n";die;}
-    if($stmt->fetch()){echo "Utilisateur Gandalf présent.\n"; $stmt->closeCursor();}
-    else{echo "Utilisateur Gandalf introuvable";} */
-?>
